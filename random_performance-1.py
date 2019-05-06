bdd4121f9954a653e14d32edbb966100e4203a79
gasm0ney <45432806+gasm0ney@users.noreply.github.com>
2019-05-06 09:49:13 -0500

    Last nights version + list of emotions
    - added list of emotions, but functionality not implemented
    - added more monologues
    - added more movements
    - unison function needs to be updated - should apply to any and all other functions 
    - need sound function (if possible)

---
+++ random_performance-1.py
@@ -0,0 +1,147 @@
+from random import *
+import time
+
+# Number of nodes
+chorus_size = 3
+
+# Possible objects. All have equal probability to be picked
+objects = ["PDF", "your favorite writing tool", "plastic fork"]
+
+# Possible commands. All have equal probability to be picked
+commands = ["monologue", "scan", "verbose", "literal", "copy", "louder", "softer", "unison", "return to backstage", "movement", "movement", "movement", "movement", "movement"]
+monologue = ["Philosophers can never hope finally to formulate these metaphysical first principles. Weakness of insight and deficiencies of language stand in the way inexorably. Words and phrases must be stretched towards a generality foreign to their ordinary usage; and however such elements of language be stabilized as technicalities, they remain metaphors mutely appealing for an imaginative leap.", "Both linear and dialectical causality no longer function, therefore everything is in determination. The center of meaning is empty, therefore we are satellites in lost orbit. We can no longer act like legislator-subjects or be passive like slaves, therefore we are sponges. Images are no longer anchored by representation, therefore they float weightless in hyperspace. Words are no longer univocal, therefore signifiers slip chaotically over each other. A circuit has been created between the real and the imaginary, therefore reality has imploded into the undecidable proximity of hyperreality.", "Case was twenty-four. At twenty-two, he’d been a cowboy a rustler, one of the best in the Sprawl. He’d been trained by the best, by McCoy Pauley and Bobby Quine, legends in the biz. He’d operated on an almost permanent adrenaline high, a byproduct of youth and proficiency, jacked into a custom cyberspace deck that projected his disembodied consciousness into the con sensual hallucination that was the matrix. A thief he’d worked for other, wealthier thieves, employers who provided the exotic software required to penetrate the bright walls of corporate systems, opening windows into rich fields of data. He’d made the classic mistake, the one he’d sworn he’d never make. He stole from his employers.", "One. In the ample checked fur in the back and in the house, in the by next cloth and inner, in the chest, in mean wind. One. In the best most silk and water much, in the best most silk. One. In the best might last and wind that. In the best might last and wind in the best might last. Ages, ages, all what sat. One. In the gold presently, in the gold presently unsuddenly and decapsized and dewalking. In the gold coming in. Two. A touching white shining sash and a touching white green undercoat and a touching white colored orange and a touching piece of elastic. A touching piece of elastic suddenly. A touching white inlined ruddy hurry, a touching research in all may day. A touching research is an over show. A touching expartition is in an example of work, a touching beat is in the best way. A touching box is in a coach seat so that a touching box is on a coach seat so a touching box is on a coach seat, a touching box is on a coat seat, a touching box is on a coach seat. A touching box is on the touching so helping held. Two. Any left in the touch is a scene, a scene. Any left in is left somehow. Four. Four between, four between and hacking. Four between and hacking. Five. Four between and a saddle, a kind of dim judge and a great big so colored dog."]
+
+unison_text = "This is a test"
+
+#Adam - I just wanted to add this here - do with it what you will! (AG)
+emotions = ["realize you are angry," "you have been betrayed", "this is the best news you've ever gotten", "sing the song of your heartbreak", "you are disconsolate", "resigned", "hopeless", "radiate kindness", "you are a doctor. break the bad news gently", "convince me", "squish the audience with your words" ]
+
+movements = [
+"Dab.",
+"Escape your skin.",
+"Get stuck in the net.",
+"Do anything that makes it hard to breathe.",
+"Find your breath. Breathe.",
+"Lay down and look at your phone.",
+"Explore the most uncomfortable way to look at your phone. Maintain eye contact with phone at all costs.",
+"Oscillate between \"phone posture\" and \"good posture.\"",
+"awkward turtle",
+"laughing while crying emoji",
+"me gusta meme",
+"become furniture"
+]
+
+monologue = monologue.split(" ")
+unison_text = unison_text.split(" ")
+
+# Array of what nodes are currently doing
+chorus_states = ["Empty"]
+for i in range(1, chorus_size+1):
+    chorus_states.append("phone")
+    
+# Times in seconds
+initial_delay_min = 30
+initial_delay_max = 90
+unison_time = 15
+loop_delay_min = 5
+loop_delay_max = 15
+
+# Maximum amount of time the program will run
+max_time = 300
+
+# Set's start time to when the for loop begins
+start_time = time.time()
+
+# Initial delay
+wait = randint(initial_delay_min, initial_delay_max)
+#time.sleep(wait)
+
+# Integer used to exit the main while loop
+exit = 0
+
+while(exit == 0 and (time.time() - start_time) < max_time):
+    # Picks a random command and node
+    shuffle(commands)
+    node_number = randint(1, chorus_size)
+    issued_command = 0
+    
+    # If unison picked, have a random node conduct for a fixed amount of time.
+    # Then, all return to phones once finished.
+    # Thought: This is still human conducted although conductor randomly picked
+    if(commands[0] == "unison"):
+		print("All nodes in unison")
+	    time.sleep(1)
+	    for i in range(0, len(unison_text)):
+			print(unison_text[i])
+			time.sleep(0.5)
+		for i in range(1, chorus_size+1):
+	    	chorus_states[i] = "phone"
+		time.sleep(1)
+	    issued_command = 1
+
+    if(commands[0] == "monologue"):
+    	print("Node #" + str(node_number) + " –- " + commands[0])
+    	for i in range(0, len(monologue)):
+    		print(monologue[i])
+    		time.sleep(0.5)
+    	for i in range(1, chorus_size+1):
+            chorus_states[i] = "phone"
+    	time.sleep(1)
+    	issued_command = 1
+    
+    # Tells the selected node to scan and sets them to active
+    # Thoughts: This setup will probably lead to lengthier scans than we've been doing
+    if(commands[0] == "scan"):
+        print("Node #" + str(node_number) + " –- " + commands[0])
+        chorus_states[node_number] = "active"
+        issued_command = 1
+        
+    # If a node is active and "return to backstage", "softer", or "louder"
+    # selected, issues command to random active node
+    if((commands[0] == "return to backstage" or commands[0] == "louder" or commands[0] == "softer") and "active" in chorus_states):
+        while(chorus_states[node_number] != "active"):
+            node_number = randint(1, chorus_size)
+        print("Node #" + str(node_number) + " –- " + commands[0])
+        # If returning to backstage, sets state to "phone"
+        if(commands[0] == "return to backstage"):
+            chorus_states[node_number] = "phone"
+        issued_command = 1
+            
+    # Tells selected node to literal or verbose a random object
+    # Thoughts: Can have two nodes working on same object at once
+    if(commands[0] == "verbose" or commands[0] == "literal"):
+        shuffle(objects)
+        print("Node #" + str(node_number) + " –- " + commands[0] + " " + objects[0])
+        chorus_states[node_number] = "active"
+        issued_command = 1
+        
+    # Picks random active node and tells a different node to copy it
+    # Thoughts: Can now copy scan, just for simplicity
+    if(commands[0] == "copy" and "active" in chorus_states):
+        target_number = randint(1, chorus_size)
+        while(chorus_states[target_number] != "active"):
+            target_number = randint(1, chorus_size)
+        while node_number == target_number:
+            node_number = randint(1, chorus_size)
+        print("Node #" + str(node_number) + " –- " + commands[0] + " Node #" + str(target_number))
+        chorus_states[node_number] = "copying"
+        issued_command = 1
+
+    if(commands[0] == "movement"):
+    	shuffle(movements)
+    	while(movements[0] == "Dab."):
+    		if(randint(0,5) == 3):
+    			break
+    		shuffle(movements)
+    	print("Node #" + str(node_number) + " –- " + movements[0])
+    	chorus_states[node_number] = "moving"
+    	issued_command = 1
+    
+    if issued_command:
+        # Wait time before issuing another command
+        wait = randint(loop_delay_min, loop_delay_max)
+        time.sleep(wait)
+        
+        # 2% chance of quiting
+        if(randint(0,50) == 25):
+            exit = 1
